e6155adeef3b4d74be1a7740018d1e9e3788a8aa
izahonorato <izahonorato@gmail.com>
2021-05-30 17:39:19 -0300

    tratamento de erro

--- src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/controllers/LoginController.kt
+++ src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/controllers/LoginController.kt
@@ -11,16 +11,21 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/login")
-
 class LoginController {
 
     @PostMapping
-    fun efetuarLogin(@RequestBody objeto : LoginDTO) : ResponseEntity<Any>{
+    fun efetuarLogin(@RequestBody dto : LoginDTO) : ResponseEntity<Any>{
         try{
-            throw RuntimeException("Testando uma exceção")
+            if(dto == null || dto.login.isNullOrBlank() || dto.login.isNullOrEmpty()
+                || dto.senha.isNullOrEmpty() || dto.senha.isNullOrBlank()){
+                return ResponseEntity(ErroDTO(HttpStatus.BAD_REQUEST.value(),
+                    "Parâmetros de entrada inválidos"), HttpStatus.BAD_REQUEST)
+            }
+
+            return ResponseEntity("Login autenticado com sucesso", HttpStatus.OK)
         }catch (e: Exception){
-            return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Não foi possível efetuar o login, tente novamente"), HttpStatus.INTERNAL_SERVER_ERROR)
+            return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Não foi possível efetuar o login, tente novamente"), HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
 }
